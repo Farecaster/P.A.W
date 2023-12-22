0875435c5b3b12eaccd2be8757f73edc45ccd0a3
nathan <nathanmanzano@gmail.com>
2023-12-22 21:17:50 +0800

    firstcommit

---
+++ app/src/main/java/com/example/manzano/nathanielree/block4/p1/paw_adopt_paw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.manzano.nathanielree.block4.p1.paw_adopt_paw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
